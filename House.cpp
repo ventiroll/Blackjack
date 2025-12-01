c0b77d3725c83e5ed60bc4ad02e0f268b150e51c
jclie <jtong0003@mymail.lausd.net>
2025-12-01 01:11:42 -0800

    Completedish House.cpp and Player.cpp

--- House.cpp
+++ House.cpp
@@ -1,1 +1,33 @@
-// inherit the class GenericPlayer and displays the status (Hitting, win, lose etc) of the house 
+#include "House.h"
+#include <iostream>
+
+// constructor
+House::House(const std::string& name) : GenericPlayer(name) {}
+
+// destructor
+House::~House() {}
+
+// determines if dealer should get another card
+// dealer should hit on 16 or less
+// stay given 17 or more
+bool House::isHitting() const {
+    // get dealer current total
+    int total = getTotal();
+
+    if (total <= 16) {
+        return true; // if dealer <= 16, get another card
+    } else {
+        return false; // if dealer >= 17, stay
+    }
+}
+
+// flip dealer first card
+// check that there is at least one card first
+void House::flipFirstCard() const {
+    // check if no cards
+    if (!cardVector.empty()) { // change to card vector name
+        cardVector[0]->flip();
+    } else {
+        std::cout << "No card to flip!" << std::endl;
+    }
+}
--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,37 @@
-// inherit the class GenericPlayer and displays the status (Hitting, win, lose etc) of each player
+#include "Player.h"
+#include <iostream>
+
+// constructor: initializes player with the given name
+Player::Player(const std::string& name) : GenericPlayer(name) {
+    // GenericPlayer handles initialization, nothing needed here
+}
+
+// destructor
+Player::~Player() {}
+
+// asks/checks if player wants to hit
+bool Player::isHitting() const {
+    std::cout<< name << ", do you want to hit? (y or n ONLY):";
+    char response;
+    std::cin>>response;
+    if (response == 'y' || response == 'Y') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// declares that player has won
+void Player::win() const {
+    std::cout << name << " wins!" << std::endl;
+}
+
+// declares player has lost
+void Player::lose() const {
+    std::cout << name << "loses." << std::endl;
+}
+
+// announces a push
+void Player::push() const {
+    std::cout << name << "pushes." << std::endl;
+}
